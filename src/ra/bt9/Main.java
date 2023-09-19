b3fbe878097ee0afd1d39127d9cdd71a0b74f4dc
Nguyễn Xuân Thành <nxt1012@gmail.com>
2023-09-19 23:27:23 +0700

    update find the second largest number algorythm

--- src/ra/bt9/Main.java
+++ src/ra/bt9/Main.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {1, 2, 4, 7, 4, 9};
-        int max = arr[0];
-        int max2 = 0;
+        int[] arr = {10, 2, 4, 7, 4, 9};
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
         for (int j : arr) {
             if (j > max) {
                 max2 = max;
                 max = j;
+            } else if (j > max2 && j != max){
+                max2 = j;
             }
         }
         System.out.println("Phần tử lớn thứ hai trong mảng là: " + max2);
